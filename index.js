9eb8625818d5acc135afd727ebd8dc99556b038f
donellgill <donell.gill@outlook.com>
2021-03-21 23:47:26 -0400

    completed assignment

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const main = document.querySelector('#main');
 main.remove();
 
 var newHeader = document.createElement('h1');
-document.getElementsByTagName('h1')[0].setAttribute('id','victory');
-
-newHeader.nodeName('H1');
 newHeader.id = 'victory';
+
 newHeader.innerHTML = 'Donell is the champion!';
 
 
